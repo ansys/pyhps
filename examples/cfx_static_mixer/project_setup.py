2c68b6d83339564574cabbcab0b487973a2c56d5
Dave Logie <dave.logie@ansys.com>
2022-09-19 13:52:35 -0400

    code style

--- examples/cfx_static_mixer/project_setup.py
+++ examples/cfx_static_mixer/project_setup.py
@@ -4,7 +4,6 @@ Example script to setup a simple CFX project in pyrep.
 import argparse
 import logging
 import os
-import random
 
 from ansys.rep.client import Client, REPError
 from ansys.rep.client import __external_version__ as ansys_version
@@ -19,10 +18,11 @@ from ansys.rep.client.jms import (
     ResourceRequirements,
     Software,
     SuccessCriteria,
-    TaskDefinition
+    TaskDefinition,
 )
 
 log = logging.getLogger(__name__)
+
 
 def create_project(client, name, num_jobs=20, use_exec_script=False):
     """
@@ -39,25 +39,57 @@ def create_project(client, name, num_jobs=20, use_exec_script=False):
     cwd = os.path.dirname(__file__)
     files = []
     files.append(
-        File(name="ccl", evaluation_path="runInput.ccl", type="text/plain", src=os.path.join(cwd, "runInput.ccl") )
+        File(
+            name="ccl",
+            evaluation_path="runInput.ccl",
+            type="text/plain",
+            src=os.path.join(cwd, "runInput.ccl"),
+        )
     )
     files.append(
-        File(name="inp", evaluation_path="StaticMixer_001.cfx", type="application/octet-stream", src=os.path.join(cwd, "StaticMixer_001.cfx") )
+        File(
+            name="inp",
+            evaluation_path="StaticMixer_001.cfx",
+            type="application/octet-stream",
+            src=os.path.join(cwd, "StaticMixer_001.cfx"),
+        )
     )
     files.append(
-        File(name="def", evaluation_path="StaticMixer_001.def", type="application/octet-stream", src=os.path.join(cwd, "StaticMixer_001.def") )
+        File(
+            name="def",
+            evaluation_path="StaticMixer_001.def",
+            type="application/octet-stream",
+            src=os.path.join(cwd, "StaticMixer_001.def"),
+        )
     )
 
     if use_exec_script:
         files.append(
-            File(name="exec_cfx", evaluation_path="exec_cfx.py", type="application/x-python-code", src=os.path.join(cwd, "..", "exec_scripts", "exec_cfx.py") )
+            File(
+                name="exec_cfx",
+                evaluation_path="exec_cfx.py",
+                type="application/x-python-code",
+                src=os.path.join(cwd, "..", "exec_scripts", "exec_cfx.py"),
+            )
         )
 
     files.append(
-        File(name="out", evaluation_path="StaticMixer_*.out", type="text/plain", collect=True, monitor=True)
+        File(
+            name="out",
+            evaluation_path="StaticMixer_*.out",
+            type="text/plain",
+            collect=True,
+            monitor=True,
+        )
     )
     files.append(
-        File(name="res", evaluation_path="StaticMixer_*.res", type="text/plain", collect=True, monitor=False)
+        File(
+            name="res",
+            evaluation_path="StaticMixer_*.res",
+            type="text/plain",
+            collect=True,
+            monitor=False,
+        )
     )
 
     files = project_api.create_files(files)
@@ -65,7 +97,7 @@ def create_project(client, name, num_jobs=20, use_exec_script=False):
 
     log.debug("=== JobDefinition with simulation workflow and parameters")
     job_def = JobDefinition(name="JobDefinition.1", active=True)
-    
+
     # Task definition
     num_input_files = 4 if use_exec_script else 3
     task_def = TaskDefinition(
@@ -73,17 +105,14 @@ def create_project(client, name, num_jobs=20, use_exec_script=False):
         software_requirements=[
             Software(name="Ansys CFX", version=ansys_version),
         ],
-        execution_command=None, # only execution script supported initially
+        execution_command=None,  # only execution script supported initially
         resource_requirements=ResourceRequirements(
             cpu_core_usage=1.0,
             memory=250,
             disk_space=5,
         ),
         execution_level=0,
-        execution_context = {
-            "cfx_cclFile" : "runInput.ccl",
-            "cfx_runName" : "StaticMixer"
-        },
+        execution_context={"cfx_cclFile": "runInput.ccl", "cfx_runName": "StaticMixer"},
         max_execution_time=50.0,
         num_trials=1,
         input_file_ids=[f.id for f in files[:num_input_files]],
@@ -114,14 +143,13 @@ def create_project(client, name, num_jobs=20, use_exec_script=False):
     log.debug(f"=== Create {num_jobs} jobs")
     jobs = []
     for i in range(num_jobs):
-        jobs.append(
-            Job(name=f"Job.{i}", eval_status="pending", job_definition_id=job_def.id)
-        )
+        jobs.append(Job(name=f"Job.{i}", eval_status="pending", job_definition_id=job_def.id))
     jobs = project_api.create_jobs(jobs)
 
     log.info(f"Created project '{proj.name}', ID='{proj.id}'")
 
     return proj
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
