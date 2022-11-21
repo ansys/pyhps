41e665a17558461d42b8014576e5c7670ab1de2c
Federico Negri <federico.negri@ansys.com>
2022-11-21 14:34:56 +0100

    Add test for task definition fields

--- tests/jms/test_job_definitions.py
+++ tests/jms/test_job_definitions.py
@@ -1,7 +1,12 @@
 import logging
 
 from ansys.rep.client.jms import JmsApi, ProjectApi
-from ansys.rep.client.jms.resource import JobDefinition, Project
+from ansys.rep.client.jms.resource import (
+    JobDefinition,
+    Project,
+    ResourceRequirements,
+    TaskDefinition,
+)
 from tests.rep_test import REPTestCase
 
 log = logging.getLogger(__name__)
@@ -27,3 +32,36 @@ class JobDefinitionsTest(REPTestCase):
         assert len(project_api.get_job_definitions()) == 0
 
         jms_api.delete_project(proj)
+
+    def test_task_definition_fields(self):
+
+        # verify that:
+        # - store_output is defaulted to True when undefined,
+        # - memory and disk_space are correctly stored in bytes
+
+        client = self.client()
+        jms_api = JmsApi(client)
+        proj_name = f"test_store_output"
+
+        project = Project(name=proj_name, active=False, priority=10)
+        project = jms_api.create_project(project)
+        project_api = ProjectApi(client, project.id)
+
+        task_def = TaskDefinition(
+            name="Task.1",
+            application_name="MyApp",
+            application_version="1.0.0",
+            execution_command="%executable%",
+            max_execution_time=10.0,
+            execution_level=0,
+            resource_requirements=ResourceRequirements(
+                memory=256 * 1024 * 1024 * 1024,  # 256GB
+                disk_space=2 * 1024 * 1024 * 1024 * 1024,  # 2TB
+            ),
+        )
+        task_def = project_api.create_task_definitions([task_def])[0]
+        self.assertEqual(task_def.store_output, True)
+        self.assertEqual(task_def.resource_requirements.memory, 256 * 1024 * 1024 * 1024)
+        self.assertEqual(task_def.resource_requirements.disk_space, 2 * 1024 * 1024 * 1024 * 1024)
+
+        jms_api.delete_project(project)
