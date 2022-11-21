4224831e39599414e8e79a2ca8948ad4003df2a7
Federico Negri <federico.negri@ansys.com>
2022-11-21 16:58:26 +0100

    Update external version

--- ansys/rep/client/__version__.py
+++ ansys/rep/client/__version__.py
@@ -1,14 +1,6 @@
-# ----------------------------------------------------------
-# Copyright (C) 2019 by
-# ANSYS Switzerland GmbH
-# www.ansys.com
-#
-# Author(s): F.Negri
-# ----------------------------------------------------------
-
 __version__ = "0.1.dev0"
-__external_version__ = "2023 R1"
-__version_no_dots__ = "231"
+__external_version__ = "2023 R2"
+__version_no_dots__ = "232"
 __company__ = "ANSYS, Inc."
 __company_short__ = "Ansys"
 __url__ = "https://github.com/pyansys/pyrep"
