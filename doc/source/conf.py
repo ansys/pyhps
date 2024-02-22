1bb7e1c5816492fd8426630866e5b0f9f02c68f6
Roberto Pastor Muela <37798125+RobPasMue@users.noreply.github.com>
2024-02-22 08:19:34 +0100

    docs: switcher link broken

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -173,7 +173,7 @@ html_theme_options = {
     "navigation_depth": 5,
     "check_switcher": False,
     "switcher": {
-        "json_url": f"https://{cname}/release/versions.json",  # noqa: E231
+        "json_url": f"https://{cname}/versions.json",  # noqa: E231
         "version_match": get_version_match(__version__),
     },
     "navbar_end": ["version-switcher", "theme-switcher", "navbar-icon-links"],
