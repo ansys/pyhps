970c75c02903d2c80474e364c2cf6f50e2796e6c
Oliver Koenig <oliver.koenig@ansys.com>
2024-11-20 21:27:47 +0100

    Moving exec_python.py back into individual examples subfolders to keep them self-contained

--- examples/python_large_output/project_setup.py
+++ examples/python_large_output/project_setup.py
@@ -82,7 +82,7 @@ def main(client, use_exec_script) -> Project:
                 name="exec_python",
                 evaluation_path="exec_python.py",
                 type="application/x-python-code",
-                src=os.path.join(cwd, "..", "exec_python.py"),
+                src=os.path.join(cwd, "exec_python.py"),
             )
         )
 
