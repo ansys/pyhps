912289b7e2f26ee6074422ffb3ae806083c00ed4
Federico Negri <FedericoNegri@users.noreply.github.com>
2025-01-06 08:27:24 +0100

    Update license year (#521)

--- examples/mapdl_motorbike_frame/project_setup.py
+++ examples/mapdl_motorbike_frame/project_setup.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2022 - 2024 ANSYS, Inc. and/or its affiliates.
+# Copyright (C) 2022 - 2025 ANSYS, Inc. and/or its affiliates.
 # SPDX-License-Identifier: MIT
 #
 #
@@ -352,7 +352,7 @@ if __name__ == "__main__":
     parser.add_argument("-n", "--name", type=str, default="Mapdl Motorbike Frame")
     parser.add_argument("-j", "--num-jobs", type=int, default=50)
     parser.add_argument("-es", "--use-exec-script", default=False, action="store_true")
-    parser.add_argument("-U", "--url", default="https://127.0.0.1:8443/hps")
+    parser.add_argument("-U", "--url", default="https://10.231.106.165:3000/hps")
     parser.add_argument("-u", "--username", default="repuser")
     parser.add_argument("-p", "--password", default="repuser")
     parser.add_argument("-v", "--ansys-version", default=__ansys_apps_version__)
