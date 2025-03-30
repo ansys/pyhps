7e04d69932441857a6bfca9ec408e9a1865c24a6
Jon Novak <jon.novak@ansys.com>
2025-03-30 15:59:18 -0500

    latest

--- examples/mapdl_tyre_performance/project_setup.py
+++ examples/mapdl_tyre_performance/project_setup.py
@@ -121,6 +121,7 @@ def create_project(
             lower_limit=-8.0,
             upper_limit=8.0,
             default=0.0,
+            mode="input",
         ),
         FloatParameterDefinition(
             name="inflation_pressure",
@@ -128,6 +129,7 @@ def create_project(
             lower_limit=0.15e06,
             upper_limit=0.3e06,
             default=0.24e06,
+            mode="input",
         ),
         FloatParameterDefinition(
             name="rotational_velocity",
@@ -135,6 +137,7 @@ def create_project(
             lower_limit=0.0,
             upper_limit=70.0,
             default=50.0,
+            mode="input",
         ),
         FloatParameterDefinition(
             name="translational_velocity",
@@ -142,6 +145,7 @@ def create_project(
             lower_limit=0.0,
             upper_limit=30.0,
             default=20.0,
+            mode="input",
         ),
     ]
     input_params = project_api.create_parameter_definitions(input_params)
@@ -177,8 +181,10 @@ def create_project(
 
     # Collect some runtime stats from MAPDL out file
     output_params = [
-        FloatParameterDefinition(name="mapdl_cp_time", display_text="MAPDL CP Time"),
-        FloatParameterDefinition(name="mapdl_elapsed_time", display_text="MAPDL Elapsed Time"),
+        FloatParameterDefinition(name="mapdl_cp_time", display_text="MAPDL CP Time", mode="output"),
+        FloatParameterDefinition(
+            name="mapdl_elapsed_time", display_text="MAPDL Elapsed Time", mode="output"
+        ),
     ]
     output_params = project_api.create_parameter_definitions(output_params)
 
