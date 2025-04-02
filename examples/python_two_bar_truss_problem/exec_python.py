c8e1456de405eeb1862d45a9990bfd7696d5b4d9
Pascal Engeler <pascal.engeler@ansys.com>
2025-04-02 14:03:23 +0200

    Rolled back exec script

---
+++ examples/python_two_bar_truss_problem/exec_python.py
@@ -0,0 +1,69 @@
+# Copyright (C) 2022 - 2025 ANSYS, Inc. and/or its affiliates.
+# SPDX-License-Identifier: MIT
+#
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+"""Simplistic execution script for Python.
+
+Command formed: python <script_file> <input_file (optional)>
+"""
+
+import os
+
+from ansys.rep.common.logging import log
+from ansys.rep.evaluator.task_manager import ApplicationExecution
+
+
+class PythonExecution(ApplicationExecution):
+    def execute(self):
+        log.info("Start Python execution script")
+
+        # Identify files
+        script_file = next((f for f in self.context.input_files if f["name"] == "script"), None)
+        assert script_file, "Python script file script missing"
+        inp_file = next((f for f in self.context.input_files if f["name"] == "inp"), None)
+        # assert inp_file, "Input file inp missing"
+
+        # Identify application
+        app_name = "Python"
+        app = next((a for a in self.context.software if a["name"] == app_name), None)
+        assert app, f"Cannot find app {app_name}"
+
+        # Add " around exe if needed for Windows
+        exe = app["executable"]
+        if " " in exe and not exe.startswith('"'):
+            exe = '"%s"' % exe  # noqa
+
+        # Use properties from resource requirements
+        # None currently
+
+        # Pass env vars correctly
+        env = dict(os.environ)
+        env.update(self.context.environment)
+
+        # Form command
+        cmd = f"{exe} {script_file['path']}"
+        if inp_file:
+            cmd += f" {inp_file['path']}"
+
+        # Execute
+        self.run_and_capture_output(cmd, shell=True, env=env)
+
+        log.info("End Python execution script")
