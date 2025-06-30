92f914f6e365d798fe3b7a990238fd2f79d63592
Pascal Engeler <pascal.engeler@ansys.com>
2025-06-30 15:28:23 +0200

    Fixed command

--- examples/python_uv/exec_script.py
+++ examples/python_uv/exec_script.py
@@ -20,9 +20,9 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-"""Simplistic execution script for Python with uv.
+"""Simple execution script for Python with uv.
 
-Command formed: uv run <script_file> <input_file>
+Command formed: uv run <script_file>
 """
 
 import json
