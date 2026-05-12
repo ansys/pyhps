453ea28a4a411c06fc1481ddf88e3bc7279e8db1
Michal Pawlik <mpawlik@synopsys.com>
2026-05-12 11:04:21 +0200

    Skip task def template permission tests for now

--- tests/jms/test_task_definition_templates.py
+++ tests/jms/test_task_definition_templates.py
@@ -25,6 +25,7 @@ import logging
 import uuid
 
 from marshmallow.utils import missing
+import pytest
 
 from ansys.hps.client import HPSError
 from ansys.hps.client.auth import AuthApi
@@ -200,6 +201,7 @@ def test_template_integration(jms_api, request):
     jms_api.delete_task_definition_templates([new_template])
 
 
+@pytest.mark.skip("not in mk2 at the moment")
 def test_template_permissions(client, keycloak_client, is_admin, request):
     jms_api = JmsApi(client)
 
@@ -296,7 +298,7 @@ def test_template_permissions(client, keycloak_client, is_admin, request):
     # Delete user
     delete_user(keycloak_client, user1)
 
-
+@pytest.mark.skip("not in mk2 at the moment")
 def test_template_permissions_update(jms_api, request):
     xfail_for_hps_version_under(HpsRelease.v1_3_45, jms_api, request)
 
@@ -322,6 +324,7 @@ def test_template_permissions_update(jms_api, request):
     jms_api.delete_task_definition_templates([template])
 
 
+@pytest.mark.skip("not in mk2 at the moment")
 def test_template_anyone_permission(client, keycloak_client, request):
     jms_api = JmsApi(client)
     xfail_for_hps_version_under(HpsRelease.v1_3_45, jms_api, request)
@@ -386,6 +389,7 @@ def test_template_anyone_permission(client, keycloak_client, request):
     delete_user(keycloak_client, user1)
 
 
+@pytest.mark.skip("not in mk2 at the moment")
 def test_template_delete(client, keycloak_client, request):
     xfail_for_hps_version_under(HpsRelease.v1_3_45, JmsApi(client), request)
 
